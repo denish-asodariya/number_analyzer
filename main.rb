5393d1dc2f9265aa7404ec6aab7f9ebaaf1309ab
DENISH ASODARIYA <153240040+denish-asodariya@users.noreply.github.com>
2024-05-08 23:38:35 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,17 +2,28 @@ require_relative 'lib/number_analyzer'
 
 def print_welcome_message
   puts "Welcome to Number Analyzer CLI!"
-  puts "Please enter a string to analyze:"
 end
 
 def get_user_input
-  print "> "
+  print "Please enter a string to analyze: "
   gets.chomp
 end
 
 def analyze_input(input)
   analyzer = NumberAnalyzer.new(input)
-  analyzer.analyze
+  analysis_results = analyzer.analyze
+  display_results(analysis_results)
+end
+
+def display_results(results)
+  puts "\nAnalysis for the input string:"
+  puts "Number of letters: #{results[:letters_count]}"
+  puts "Number of spaces: #{results[:spaces_count]}"
+  puts "Number of digits: #{results[:digits_count]}"
+  puts "Number of symbols: #{results[:symbols_count]}"
+  puts "Total characters: #{results[:total_characters]}"
+  puts "Word frequencies: #{results[:word_frequencies]}"
+  puts "Sentiment: #{results[:sentiment]}"
 end
 
 def main
